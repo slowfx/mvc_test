e6e3c8e8fd3f338a176449c13f4dd4fcb0445a2a
slowfx <contact@slowfx.com>
2014-06-06 19:00:15 +0900

    first commit

---
+++ mvc_test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_test.Startup))]
+namespace mvc_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
